32b370be489ec6ebee1de2ae81edbce4f1344344
Charles Averill <charlesaverill20@gmail.com>
2023-07-20 04:43:41 -0500

    Expand UI, have it return a Board

--- gofr/board/board_graphics.ml
+++ gofr/board/board_graphics.ml
@@ -1,99 +1,163 @@
 open Graphics
 open Textures
+open Board_definitions
 
 let xdim, ydim = (720, 720)
+let expanded_xdim = int_of_float (Float.mul (float_of_int xdim) 1.5)
+let rxdim, rydim = (expanded_xdim - xdim, ydim)
+let text_xmargin, text_ymargin = (5, 5)
 let board_dim = 19
-let _ = open_graph (Printf.sprintf " %dx%d" xdim ydim)
-let _ = set_window_title "GoFR Interface"
-let board_color = 0xF5F5DC
+let fontsize = 32
 
-let point x y =
-  (* let round_down n = n / 10 * 10 in
-     if round_down x mod 20 = 0 && round_down y mod 20 = 0 then set_color beige
-     else set_color black; *)
-  set_color white;
-  fill_rect x y 2 2
+let replace_at x y (arr : color array array) (new_value : color) =
+  let row_length = Array.length arr.(0) in
+  if x >= 0 && x < row_length && y >= 0 && y < Array.length arr then
+    let _ = arr.(y).(x) <- new_value in
+    arr
+  else arr
 
-(* let img = Image.file "example_image.png" *)
-(* Draw Gameboard *)
-let _ =
-  let cornerx, cornery = (xdim / board_dim / 2, ydim / board_dim / 2) in
-  draw_image (gameboard_720 ()) 0 0;
-  set_line_width 2;
-  draw_rect cornerx cornery
-    (xdim * (board_dim - 1) / board_dim)
-    (ydim * (board_dim - 1) / board_dim);
-  (* Horizontal lines *)
-  for x = 0 to board_dim do
-    moveto (cornerx + (x * xdim / board_dim)) cornery;
-    lineto
-      (cornerx + (x * xdim / board_dim))
-      (cornery + (ydim * (board_dim - 1) / board_dim))
+let dither (img : image) : image =
+  let carr = ref (dump_image img) in
+  let dither_n = 2 in
+  for i = 0 to Array.length !carr do
+    for j = 0 to Array.length !carr.(0) do
+      if i mod dither_n = 0 && j mod dither_n = 0 then
+        carr := replace_at i j !carr transp
+      else carr := !carr
+    done
   done;
-  (* Vertical lines *)
-  for y = 0 to board_dim do
-    moveto cornerx (cornery + (y * ydim / board_dim));
+  make_image !carr
+
+let cornerx, cornery = (xdim / board_dim / 3, ydim / board_dim / 3)
+
+let board_coords_to_screen_coords x y =
+  ( cornerx + (x * (xdim - (2 * cornerx)) / board_dim),
+    cornery + (y * (ydim - (2 * cornery)) / board_dim) )
+
+let screen_coords_to_board_coords x y =
+  ((x + cornerx) * board_dim / xdim, (y + cornery) * board_dim / ydim)
+
+let screen_coords_to_board_screen_coords x y =
+  let sx, sy = screen_coords_to_board_coords x y in
+  board_coords_to_screen_coords sx sy
+
+let in_bounds x y = 0 <= x && x <= xdim && 0 <= y && y <= ydim
+
+let init_gui () =
+  open_graph (Printf.sprintf " %dx%d" expanded_xdim ydim);
+  set_window_title "GoFR Interface";
+
+  (* Draw Gameboard *)
+  let _ =
+    (* Background *)
+    draw_image (gameboard_720 ()) 0 0;
+    set_line_width 2;
+    (* Vertical lines *)
+    for x = 0 to board_dim do
+      moveto (cornerx + ((x * (xdim - (2 * cornerx)) / board_dim) + 1)) cornery;
+      lineto
+        (cornerx + ((x * (xdim - (2 * cornerx)) / board_dim) + 1))
+        (ydim - cornery)
+    done;
+    (* Horizontal lines *)
+    for y = 0 to board_dim do
+      moveto cornerx (cornery + (y * (ydim - (2 * cornery)) / board_dim));
+      lineto (xdim - cornerx)
+        (cornery + (y * (ydim - (2 * cornery)) / board_dim))
+    done;
+    (* Navigation dots *)
+    List.iter
+      (fun (x, y) ->
+        let x', y' = board_coords_to_screen_coords x y in
+        fill_circle x' y' 8)
+      [
+        (3, 3);
+        (9, 3);
+        (15, 3);
+        (3, 9);
+        (9, 9);
+        (15, 9);
+        (3, 15);
+        (9, 15);
+        (15, 15);
+      ]
+  in
+  (* Draw Register Bank *)
+  let _ =
+    draw_rect (xdim + cornerx) cornery
+      (expanded_xdim - xdim - (2 * cornerx))
+      (ydim - (2 * cornery));
+    moveto
+      (xdim + cornerx + (2 * text_xmargin))
+      (ydim - (3 * cornery) - (2 * text_ymargin));
+    set_font
+      (Printf.sprintf "-*-fixed-medium-r-semicondensed--%d-*-*-*-*-*-iso8859-1"
+         fontsize);
+    draw_string "R# Opcode N_Args Args";
+    set_line_width 3;
+    moveto (xdim + cornerx)
+      (ydim - (3 * cornery) - (2 * text_ymargin) - (fontsize / 4));
     lineto
-      (cornerx + (xdim * (board_dim - 1) / board_dim))
-      (cornery + (y * ydim / board_dim))
-  done;
-  (* Navigation dots *)
-  List.iter
-    (fun (x, y) ->
-      fill_circle
-        (cornerx + (x * xdim / board_dim))
-        (cornery + (y * ydim / board_dim))
-        8)
-    [
-      (3, 3);
-      (9, 3);
-      (15, 3);
-      (3, 9);
-      (9, 9);
-      (15, 9);
-      (3, 15);
-      (9, 15);
-      (15, 15);
-    ];
-  set_line_width 1
+      (expanded_xdim - cornerx - 1)
+      (ydim - (3 * cornery) - (2 * text_ymargin) - (fontsize / 4));
+    set_line_width 2
+  in
+  set_line_width 1;
+  remember_mode false
 
-(* let _ =
-   for y = (size_y () - 1) / 2 downto 0 do
-     for x = 0 to (size_x () - 1) / 2 do
-       point (2 * x) (2 * y)
-     done
-   done *)
+let place_and_render screen_x screen_y sprite_to_place board : fboard =
+  let boardx, boardy = screen_coords_to_board_coords screen_x screen_y in
+  match
+    place boardx boardy
+      (if sprite_to_place == black_piece_40px then Black else White)
+      board
+  with
+  | None -> board
+  | Some newboard ->
+      remember_mode true;
+      draw_image (sprite_to_place ()) screen_x screen_y;
+      remember_mode false;
+      newboard
 
-let _ = set_color (rgb 0 0 0)
-let _ = remember_mode false
-let xcursor_offset, ycursor_offset = (0, 0)
-
-let _ =
-  try
-    let place_counter = ref 0 in
-    while true do
+let draw_loop (board : fboard) : fboard =
+  let xcursor_offset, ycursor_offset = (0, 0) in
+  let rec recur counter subboard : fboard =
+    let new_board_ref = ref subboard in
+    try
+      let sprite_to_place =
+        if counter mod 2 = 0 then black_piece_40px else white_piece_40px
+      in
       let st = wait_next_event [ Mouse_motion; Button_down; Key_pressed ] in
       synchronize ();
-      if st.keypressed then raise Exit;
-      if st.button then (
-        let sprite_to_place =
-          if !place_counter mod 2 = 0 then black_piece_40px
-          else white_piece_40px
+      let snap_x, snap_y =
+        let x', y' =
+          screen_coords_to_board_screen_coords
+            (st.mouse_x - (piece_width_px / 2))
+            (st.mouse_y - (piece_width_px / 2))
         in
-        place_counter := !place_counter + 1;
-        remember_mode true;
-        draw_image (sprite_to_place ())
-          (st.mouse_x - (piece_width_px / 2))
-          (st.mouse_y - (piece_width_px / 2));
-        remember_mode false);
+        (x' + (piece_width_px / 2) - 2, y' + (piece_width_px / 2) - 2)
+      in
+      if st.keypressed then raise Exit;
+      if st.button then
+        new_board_ref :=
+          place_and_render snap_x snap_y sprite_to_place !new_board_ref;
       let x = st.mouse_x + xcursor_offset and y = st.mouse_y + ycursor_offset in
-      moveto 0 y;
-      lineto (x - 25) y;
-      moveto 10000 y;
-      lineto (x + 25) y;
-      moveto x 0;
-      lineto x (y - 25);
-      moveto x 10000;
-      lineto x (y + 25)
-    done
-  with Exit -> ()
+      let _ =
+        if in_bounds snap_x snap_y then (
+          set_color red;
+          moveto 0 y;
+          lineto (x - 25) y;
+          moveto xdim y;
+          lineto (x + 25) y;
+          moveto x 0;
+          lineto x (y - 25);
+          moveto x ydim;
+          lineto x (y + 25);
+          set_color black;
+          draw_image (dither (sprite_to_place ())) snap_x snap_y)
+        else ()
+      in
+      recur (if st.button then (counter + 1) mod 2 else counter) !new_board_ref
+    with Exit -> !new_board_ref
+  in
+  recur 0 board
--- gofr/main.ml
+++ gofr/main.ml
@@ -1,12 +1,12 @@
-(* open Board.Board_definitions
-   open Board.Board_printing *)
-open Register.Register_definitions
-open Register.Register_printing
-open Register.Operation_execution
+open Board.Board_definitions
+open Board.Board_printing
+
+(* open Register.Register_definitions
+   open Register.Register_printing
+   open Register.Operation_execution *)
 open Board.Board_graphics
 
 let _ = print_newline ()
-let _ = xdim
 
 (* let _ =
    print_board
@@ -26,15 +26,19 @@ let _ = xdim
 
    let _ = print_bank testbank *)
 
-let _ =
-  print_flowchart fresh_bank
-    [
-      set_opcode 0;
-      set_n_args 1;
-      add_arg 0;
-      incr_r;
-      set_opcode 1;
-      set_n_args 1;
-      add_arg 0;
-      (fun x -> exec (get_reg (get_r x) x) x);
-    ]
+(* let _ =
+   print_flowchart fresh_bank
+     [
+       set_opcode 0;
+       set_n_args 1;
+       add_arg 0;
+       incr_r;
+       set_opcode 1;
+       set_n_args 1;
+       add_arg 0;
+       (fun x -> exec (get_reg (get_r x) x) x);
+     ] *)
+
+let _ = init_gui ()
+let done_board = draw_loop blank_board
+let _ = print_board done_board
